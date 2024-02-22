95bcdee1d9a013206fdff06c4ed2de2e1b7cbd99
Till Wehowski <github@webfan.de>
2024-02-22 16:11:52 +0100

    Update OIDplusPagePublicCDN.js

--- OIDplusPagePublicCDN.js
+++ OIDplusPagePublicCDN.js
@@ -2,7 +2,7 @@
  var s=d.createElement('script');
  s.setAttribute('src',
 				//document.currentScript.src 
-				w.oidplus_webpath_relative + 'plugins/frdl/publicPages/cdn/OIDplusPagePublicCDN.js' + '.php');	
+				w.oidplus_webpath_absolute_canonical + 'plugins/frdl/publicPages/cdn/OIDplusPagePublicCDN.js' + '.php');	
  s.async='defer';
  d.head.appendChild(s);	
 })(window, document);
