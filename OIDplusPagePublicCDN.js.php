ed887fcf914e0f300b10353f6428263922cf984e
Till Wehowski <github@webfan.de>
2024-07-18 20:28:06 +0200

    Update OIDplusPagePublicCDN.js.php

--- OIDplusPagePublicCDN.js.php
+++ OIDplusPagePublicCDN.js.php
@@ -13,11 +13,16 @@ OIDplus::init(true);
 error_reporting(OIDplus::baseConfig()->getValue('DEBUG') ? E_ALL : 0);
 @ini_set('display_errors', OIDplus::baseConfig()->getValue('DEBUG') ? '1' : '0');
 
+error_reporting(0);
+@ini_set('display_errors','0');
+
+
 set_exception_handler(array(OIDplusGui::class, 'html_exception_handler'));
 ob_start(); 
 //  DONE ALREADY ! OIDplus::init(true);
 // session_write_close();
 OIDplus::invoke_shutdown();
+header_remove();
  originHeaders();
 header('Content-Type:application/javascript');
 //header('Connection: close');
@@ -46,17 +51,27 @@ function frdlwebJS(){
 //s.setAttribute('src', 'https://io4.xyz.webfan3.de/webfan.js?cdn=https://cdn.startdir.de&?' + q);
 	
 	$cdn = OIDplus::baseConfig()->getValue('FRDLWEB_CDN_PROXY_TARGET_BASE', OIDplusCDNProxyPagePlugin::DEFAULT_CDN_MASTER_BASEURI );
-	$cdn='https://io4.xyz.webfan3.de';
-	
+	//$cdn='https://io4.xyz.webfan3.de';
+//	$cdn='https://cdn.startdir.de';
+//	$cdn='https://io4.xyz.webfan3.de';
 	
 	    $CDN_BASEPATH =	OIDplus::baseConfig()->getValue('FRDLWEB_CDN_RELATIVE_URI', OIDplusCDNProxyPagePlugin::DEFAULT_CDN_BASEPATH );
-	    $BASE_URI = rtrim(OIDplus::webpath(OIDplus::localpath(),
+	    $BASE_URI =urlencode( rtrim(OIDplus::webpath(OIDplus::localpath(),
 										 //OIDplus::PATH_ABSOLUTE_CANONICAL
 										 OIDplus::PATH_ABSOLUTE
 										  ), '/ ').'/'.trim($CDN_BASEPATH, '/ ')
 			//.'/'
-			;	
+			);	
 	
+	
+	  $url = "$cdn/webfan.js?cdn=$BASE_URI&?$FRDLWEB_JS_CONFIG_QUERY";
+	  $file = __DIR__.\DIRECTORY_SEPARATOR.'webfan.js~cache.js';
+	  if(!file_exists($file) || filemtime($file) < time() - 24 * 60 * 60){
+		  file_put_contents($file, file_get_contents($url));
+	  }
+	
+	return file_get_contents($file);
+	/*
 	
 $jscode = <<<JSCODE
 ((q, w,d)=>{
@@ -91,5 +106,6 @@ s.onload=()=>{
 JSCODE;
  	
  return $jscode;
+ */
 }
 
